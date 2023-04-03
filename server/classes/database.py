5a844773f640c3c9e25298b39c9f5f92c92bdd37
v0ltis <40314287+v0ltis@users.noreply.github.com>
2023-04-03 19:34:38 +0200

    Updated add_score and added get_score. Not tested yet !

--- server/classes/database.py
+++ server/classes/database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from datetime import datetime
+import pydantic
 
 class Database:
     def __init__(self, host:str, database:str, user:str, password:str, port:int=3306) -> None:
@@ -15,6 +16,10 @@ class Database:
         self._cursor.execute(request, params)
         self._conn.commit()
 
+    def get (self, request, params):
+        self._cursor.execute(request, params)
+        return self._cursor.fetchall()
+
     def connect(self):
         self._conn = mysql.connector.connect(
             host = self._host,
@@ -26,5 +31,21 @@ class Database:
 
         self._cursor = self._conn.cursor()
 
-    def add_score(self, name:str, score:int, date:datetime):
-        self.execute("INSERT INTO scores (name, score, date) VALUES (%s, %s, %s)", (name, score, date))
+    def add_score(self, name:pydantic.constr(max_length=25), score:int, date:datetime, gamemode: int) -> None:
+        """
+        Add a score to the database
+        :param name: The name of the player. Must be between 1 and 25 characters.
+        :param score: The score of the player
+        :param date: The date of the score
+        :param gamemode: The gamemode of the score performed
+        :return:
+        """
+        self.execute("INSERT INTO scores (username, score, date, gamemode) VALUES (%s, %s, %s, %s)", (name, score, date, gamemode))
+
+    def get_scores(self, limit: pydantic.conint(ge=1, le=50)):
+        """
+        Get the top scores from the database
+        :param limit: The number of scores to get. Must be between 1 and 50
+        :return: A list of tuples containing the name, score and date of the scores
+        """
+        return self.get("SELECT * FROM scores ORDER BY score DESC LIMIT %s", (limit,))

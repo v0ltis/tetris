9a997d843a6dda65bfdf1fdc27ed297a69795a9b
v0ltis <40314287+v0ltis@users.noreply.github.com>
2023-04-03 19:21:55 +0200

    Added config file, an so removed the connexion in the database.py file

--- server/classes/database.py
+++ server/classes/database.py
@@ -2,11 +2,12 @@ import mysql.connector
 from datetime import datetime
 
 class Database:
-    def __init__(self, host:str, database:str, user:str, password:str) -> None:
+    def __init__(self, host:str, database:str, user:str, password:str, port:int=3306) -> None:
         self._host = host
         self._user = user
         self._password = password
         self._db = database
+        self._port = port
 
         self._cursor = None
 
@@ -17,6 +18,7 @@ class Database:
     def connect(self):
         self._conn = mysql.connector.connect(
             host = self._host,
+            port = self._port,
             user = self._user,
             password = self._password,
             database = self._db
@@ -26,14 +28,3 @@ class Database:
 
     def add_score(self, name:str, score:int, date:datetime):
         self.execute("INSERT INTO scores (name, score, date) VALUES (%s, %s, %s)", (name, score, date))
-
-
-db = Database(
-    host = "beryllium.cloud.voltis.me",
-    database="s12_tetris",
-    user="u12_iKk3mX4DKW",
-    password="7sbhV.P7mGQwPwkxISKQTC^n"
-).connect()
-
-   
-

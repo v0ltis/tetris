fd2d16766ee97dc61344086db36974422d76dbcc
v0ltis <40314287+v0ltis@users.noreply.github.com>
2023-04-04 19:17:48 +0200

    Added Gamemod classs

---
+++ menu/classes/gamemode.py
@@ -0,0 +1,71 @@
+import time
+
+class Gamemode:
+    def __init__(self, name:str, id:int,
+                 default_speed:int, speed_multiplier:float, speed_increment_every:int,
+                 pieces:list,invisible_pieces:bool=False
+                 ):
+        """
+        Creates & setup a gamemode
+        :param name: The name of the gamemode
+        :param id: The id of the gamemode
+        :param default_speed: The default speed of the gamemode
+        :param speed_multiplier: The speed multiplier
+        :param speed_increment_every: How many rounds before the speed is incremented
+        :param pieces: The pieces of the gamemode
+        :param invisible_pieces: If, once the piece is placed, it should become invisible
+        """
+
+        self.name = name
+        self.id = id
+        self.speed = default_speed
+        self.speed_multiplier = speed_multiplier
+        self.speed_increment_every = speed_increment_every
+
+        self.invisible_pieces = invisible_pieces
+        self.pieces = pieces
+
+        self.score = 0
+        self.round = 0
+        self.duration = None
+
+
+    def start(self) -> None:
+        """
+        Starts the gamemode
+        :return: None
+        """
+        self.duration = time.time()
+
+    def next_round(self) -> None:
+        """
+        Invoked after each round
+        Updates the speed if needed
+        :return: None
+        """
+
+        self.round += 1
+
+        if self.round % self.speed_increment_every == 0:
+            self.speed *= self.speed_multiplier
+
+    def get_time(self) -> float:
+        """
+        Returns the time elapsed since the gamemode started
+        :return: float
+        """
+        return time.time() - self.duration
+
+    def pause(self) -> None:
+        """
+        Pauses the gamemode
+        :return: None
+        """
+        self.pause = time.time()
+
+    def resume(self) -> None:
+        """
+        Resumes the gamemode
+        :return: None
+        """
+        self.duration += time.time() - self.pause

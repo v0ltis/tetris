3fc915dcd5e4bee72d060a494acc1385997f14c7
v0ltis <40314287+v0ltis@users.noreply.github.com>
2023-04-04 09:51:54 +0000

    Minor changes 	modified:   server/app.py

--- server/app.py
+++ server/app.py
@@ -1,13 +1,11 @@
 from tools import DB_init as db_conn
-from datetime import datetime
 
-def start():
+def start() -> None:
+    """
+    Start the server
+    """
     # Log to the database
     db = db_conn.connect()
-    
-    print(db.get_scores(3))
-
-    print(db.get_scores(3, 2))
 
 if __name__ == '__main__':
     start()

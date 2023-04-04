56cd209f40acd76930764c15b870ab162f785739
Valérian Légier-Vassard <valerian.legiervassard@gmail.com>
2023-04-04 18:02:08 +0200

    Created main menu (v1)

--- menu/menu.py
+++ menu/menu.py
@@ -1,1 +1,63 @@
+import pygame
 
+pygame.init()
+screen = pygame.display.set_mode((600,800))
+
+objects = []
+font = pygame.font.Font('tetris\\menu\\font\\upheavtt.ttf' , 40)
+
+class Button:
+    
+    def __init__(self, x, y , height, width, text="Button", function = None, one_press = False):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.function = function
+        self.one_press = one_press
+
+        self.button_surface = pygame.Surface((self.width, self.height))
+        self.button_rect = pygame.Rect((self.x, self.y, self.width, self.height))
+
+        self.button_surf = font.render(text, True, (20,20,20))
+        self.already_pressed = False
+
+        objects.append(self)
+
+    def process(self):
+        self.button_surface.fill('#ffffff')
+        mouse_pos = pygame.mouse.get_pos()
+        if self.button_rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                if self.one_press:
+                    self.function()
+                elif not self.already_pressed:
+                    self.function()
+                    self.already_pressed = True
+            else:
+                self.already_pressed = False
+        self.button_surface.blit(self.button_surf, [
+            self.button_rect.width/2 - self.button_surf.get_rect().width/2,
+            self.button_rect.height/2 - self.button_surf.get_rect().height/2
+        ])
+        screen.blit(self.button_surface, self.button_rect)
+
+def function():
+    print('It works')
+
+def quit_function():
+    pygame.quit()
+
+button_play = Button(100, 500, 75, 400, "TEST", function)
+button_settings = Button(100, 600, 75, 400, "Settings", function)
+button_quit = Button(150, 700, 50, 300, "Quit", quit_function)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    for object in objects:
+        object.process()
+
+    pygame.display.flip()

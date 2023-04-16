bf7633aa689952736d1098a467610fbb767d4b8a
v0ltis <40314287+v0ltis@users.noreply.github.com>
2023-04-16 19:49:32 +0200

    Minor changes in database system

--- server/classes/database.py
+++ server/classes/database.py
@@ -30,7 +30,7 @@ class Database:
             database = self._db
         )
 
-        self._cursor = self._conn.cursor()
+        self._cursor = self._conn.cursor(dictionary=True)
         return self
     
     def add_score(self, name:pydantic.constr(max_length=25), score:int, date:datetime, gamemode: int, duration: datetime) -> None:
@@ -40,11 +40,12 @@ class Database:
         :param score: The score of the player
         :param date: The date of the score
         :param gamemode: The gamemode of the score performed
+        :param duration: The time it took to complete the game
         :return:
         """
         self._execute("INSERT INTO scores (username, score, date, gamemode, duration) VALUES (%s, %s, %s, %s, %s)", (name, score, date, gamemode, duration))
 
-    def get_scores(self, limit: pydantic.conint(ge=1, le=50), offset: pydantic.conint(ge=0) = 0):
+    def get_scores(self, limit: pydantic.conint(ge=1, le=50), offset: pydantic.conint(ge=0) = 0) -> list[tuple[str, int, datetime]]:
         """
         Get the top scores from the database
         :param limit: The number of scores to get. Must be between 1 and 50
@@ -52,3 +53,13 @@ class Database:
         :return: A list of tuples containing the name, score and date of the scores
         """
         return self._get("SELECT * FROM scores ORDER BY score DESC LIMIT %s OFFSET %s", (limit, offset))
+
+    def get_gamemode_scores(self, gamemode: str, limit: pydantic.conint(ge=1, le=50), offset: pydantic.conint(ge=0) = 0) -> list[tuple[str, int, datetime]]:
+        """
+        Get the top scores from the database
+        :param gamemode: The gamemode of the scores to get
+        :param limit: The number of scores to get. Must be between 1 and 50
+        :param offset: The number of scores to skip. Must be greater than or equal to 0
+        :return: A list of tuples containing the name, score and date of the scores
+        """
+        return self._get("SELECT * FROM scores WHERE gamemode = %s ORDER BY score DESC LIMIT %s OFFSET %s", (gamemode, limit, offset))

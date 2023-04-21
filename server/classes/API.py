235227ff1cdab4f66b630259415ddeeeee494b0e
v0ltis <voltis.discord@gmail.com>
2023-04-21 18:27:35 +0200

    added GetUserStats class in server.class.API.py

--- server/classes/API.py
+++ server/classes/API.py
@@ -1,4 +1,19 @@
 from pydantic import BaseModel
+"""
+This file contains the classes that represent the body shape required for API requests.
+When a default value is specified, this value will be used if none is specified in the request.
+"""
+
+
+class GetUserStats(BaseModel):
+    """
+    The requestBody of the GET request for user stats
+    """
+    username: str
+    gamemode: str = "all"
+    quantity: int
+    offset: int = 0
+
 
 class GetScores(BaseModel):
     """

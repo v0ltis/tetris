632a95c571f9912b0d7b98ff22f72031b9e0b1d7
valer <valerian.legiervassard@gmail.com>
2023-05-26 10:55:50 +0200

    Tried to implement text input

--- menu/interfaces/death_screen.py
+++ menu/interfaces/death_screen.py
@@ -2,9 +2,6 @@ import pygame
 from classes.gamemode import Gamemode
 from classes.button import Button
 #from classes.text_entry import TextEntry
-from pygame.rect import Rect
-from pygame_gui.elements.ui_text_entry_line import UITextEntryLine
-import pygame_gui
 import os
 import sys
 
@@ -31,8 +28,6 @@ class DeathScreen:
         self.screen = pygame.display.set_mode((600, 800))
         pygame.display.set_caption("Triste")
 
-        manager = pygame_gui.UIManager((800, 600))
-
         button_quit = Button(
             coords=(150, 700), height=50, width=300, font=self.font,
             text="Quit", funct=self.quit_function, screen=self.screen
@@ -40,17 +35,13 @@ class DeathScreen:
 
         self.objects = [button_quit]
 
-        #text_input_box = TextEntry(150, 350, 300, self.font)
-        #self.group = pygame.sprite.Group(text_input_box)
+        self.text_enter = ""
+        self.input_active = True
 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.quit_function()
-
-                manager.process_events(event)
-
-            manager.draw_ui(self.screen)
 
             # Display the score you made
             self.display_tetris_text(self.font, "Your score: ", (300, 200), (255, 255, 255))
@@ -60,15 +51,21 @@ class DeathScreen:
             self.display_tetris_text(self.font, "Enter you name: ", (300, 300), (255, 255, 255))
             color = (72, 72, 72)
             pygame.draw.rect(self.screen, color, pygame.Rect(300, 350, 100, 100))
-            text_input = UITextEntryLine(relative_rect=Rect(300, 350, 100, 100), manager=manager)
-            #self.group.draw(self.screen)
-            #print(text_input_box.get_text())
 
             for event in pygame.event.get():
-                if event.type == pygame.USEREVENT:
-                    if event.user_type == pygame_gui.UI_TEXT_ENTRY_FINISHED:
-                        if event.ui_element == text_input:
-                            self.entered_text = event.text
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.input_active = True
+                    self.text_enter = ""
+                elif event.type == pygame.KEYDOWN and input_active:
+                    if event.key == pygame.K_RETURN:
+                        input_active = False
+                    elif event.key == pygame.K_BACKSPACE:
+                        self.text_enter =  self.text_enter[:-1]
+                    else:
+                        self.text_enter += event.unicode
+
+            self.display_tetris_text(self.font, self.text_enter, (300, 350), (255, 255, 255))
+            print(self.text_enter)
 
             # Button to go see the scoreboard ?
 
